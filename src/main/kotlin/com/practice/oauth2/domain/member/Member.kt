4d2718977fe8a8159d01d5051d5b24932b6db906
jeayoung <k01066624566@gmail.com>
2022-12-05 14:16:33 +0900

    oauth에 맞게 수정

--- src/main/kotlin/com/practice/oauth2/domain/member/Member.kt
+++ src/main/kotlin/com/practice/oauth2/domain/member/Member.kt
@@ -1,13 +1,14 @@
 package com.practice.oauth2.domain.member
 
 import jakarta.persistence.*
+import org.hibernate.annotations.DynamicUpdate
 
 
 @Entity
+@DynamicUpdate
 class Member(
     val email: String,
-    name: String,
-    password: String,
+    val name: String,
     @Enumerated(EnumType.STRING) @Column(name = "Role")
     @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "Role", joinColumns = [JoinColumn(name = "member_id")])
@@ -16,9 +17,4 @@ class Member(
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     val id: Long = 0
-
-    var password: String = password
-    private set
-    var name: String = name
-    private set
 }
