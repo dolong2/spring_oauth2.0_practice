beeebfdf73ec63fef4b053b9c72f956895eb301f
jeayoung <k01066624566@gmail.com>
2022-12-07 09:28:47 +0900

    add SecurityConfiguration

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,9 +27,9 @@ dependencies {
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
-	implementation("io.jsonwebtoken:jjwt-api:0.11.1")
-	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.1")
-	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.1")
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 }
 
---
+++ src/main/kotlin/com/practice/oauth2/infraStructure/config/security/SecurityConfiguration.kt
@@ -0,0 +1,32 @@
+package com.practice.oauth2.infraStructure.config.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.stereotype.Component
+
+
+@Component
+@EnableWebSecurity
+class SecurityConfiguration(
+    //private val oauthService: OauthService
+){
+    @Bean
+    @Throws(Exception::class)
+    fun filterChain(http: HttpSecurity): SecurityFilterChain? {
+        http
+            .csrf().disable()//csrf 공격을 막아주는 옵션을 disalbe, rest api같은 경우에는 브라우저를 통해 request 받지 않기 때문에 해당 옵션을 꺼도 됩니다.
+            .headers().frameOptions().disable()
+            .and()
+            .logout().logoutSuccessUrl("/") //logout 요청시 홈으로 이동 - 기본 logout url = "/logout"
+            .and()
+            .oauth2Login() //OAuth2 로그인 설정 시작점
+            .defaultSuccessUrl("/oauth/loginInfo", true) //OAuth2 성공시 redirect
+            .userInfoEndpoint() //OAuth2 로그인 성공 이후 사용자 정보를 가져올 때 설정 담당
+            //.userService(oAuthService)
+        return http.build()
+    }
+}
