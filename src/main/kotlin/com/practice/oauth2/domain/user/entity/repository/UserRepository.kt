a290cf6e40967ef624091485b12f830f17c3d73f
dolong2 <k01066624566@gmail.com>
2024-04-15 23:16:50 +0900

    [add] findUserBySocialIdAndSocialType 메서드 추가

--- src/main/kotlin/com/practice/oauth2/domain/user/entity/repository/UserRepository.kt
+++ src/main/kotlin/com/practice/oauth2/domain/user/entity/repository/UserRepository.kt
@@ -1,8 +1,10 @@
 package com.practice.oauth2.domain.user.entity.repository
 
 import com.practice.oauth2.domain.user.entity.User
+import com.practice.oauth2.domain.user.entity.enums.SocialType
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
 interface UserRepository : JpaRepository<User, UUID> {
+    fun findUserBySocialIdAndSocialType(socialId: String, socialType: SocialType): User?
 }
