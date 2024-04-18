722d2a15abd1da185cacee58e152c80f11ff59ed
dolong2 <k01066624566@gmail.com>
2024-04-18 23:17:30 +0900

    [add] ConfigurationPropertiesScan 어노테이션 추가

--- src/main/kotlin/com/practice/oauth2/Oauth2Application.kt
+++ src/main/kotlin/com/practice/oauth2/Oauth2Application.kt
@@ -1,9 +1,11 @@
 package com.practice.oauth2
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class Oauth2Application
 
 fun main(args: Array<String>) {
