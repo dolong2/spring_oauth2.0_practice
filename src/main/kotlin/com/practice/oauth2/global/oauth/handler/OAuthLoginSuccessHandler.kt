d7c51ce7eca44c521286542bdb359687c1dac71a
dolong2 <k01066624566@gmail.com>
2024-04-19 23:01:51 +0900

    [add] OAuth 로그인 성공시에 Token 발급 로직 추가

--- src/main/kotlin/com/practice/oauth2/global/oauth/handler/OAuthLoginSuccessHandler.kt
+++ src/main/kotlin/com/practice/oauth2/global/oauth/handler/OAuthLoginSuccessHandler.kt
@@ -1,6 +1,7 @@
 package com.practice.oauth2.global.oauth.handler
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.practice.oauth2.global.jwt.util.TokenGenerator
 import com.practice.oauth2.global.oauth.CustomOAuthUser
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
@@ -12,12 +13,16 @@ import org.springframework.stereotype.Component
 
 @Component
 class OAuthLoginSuccessHandler(
-    private val objectMapper: ObjectMapper
+    private val objectMapper: ObjectMapper,
+    private val tokenGenerator: TokenGenerator
 ) : AuthenticationSuccessHandler {
     override fun onAuthenticationSuccess(request: HttpServletRequest, response: HttpServletResponse, authentication: Authentication) {
         val oAuthUser = authentication.principal as CustomOAuthUser
 
         //TODO Token 생성하는 로직 추가
+        val accessToken = tokenGenerator.generateAccessToken(oAuthUser.userId.toString())
+        val refreshToken = tokenGenerator.generateRefreshToken(oAuthUser.userId.toString())
+        val accessExpiredTime = tokenGenerator.getAccessExpiredTime()
 
         response.status = HttpStatus.OK.value()
         response.contentType = MediaType.APPLICATION_JSON_VALUE
