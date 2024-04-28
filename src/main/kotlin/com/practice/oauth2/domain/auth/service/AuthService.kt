0b24dc1235d389edd94ca5efd49db23c723a4cd7
dolong2 <k01066624566@gmail.com>
2024-04-28 20:44:09 +0900

    [add] reissue 로직 추가

--- src/main/kotlin/com/practice/oauth2/domain/auth/service/AuthService.kt
+++ src/main/kotlin/com/practice/oauth2/domain/auth/service/AuthService.kt
@@ -9,10 +9,14 @@ import com.practice.oauth2.domain.auth.service.dto.request.SignupReqDto
 import com.practice.oauth2.domain.auth.service.dto.response.TokenResDto
 import com.practice.oauth2.domain.user.entity.repository.UserRepository
 import com.practice.oauth2.domain.user.exception.UserNotFoundException
+import com.practice.oauth2.global.jwt.exception.ExpiredTokenException
+import com.practice.oauth2.global.jwt.exception.InvalidTokenException
 import com.practice.oauth2.global.jwt.util.TokenGenerator
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime
 
 @Service
 class AuthService(
@@ -47,6 +51,28 @@ class AuthService(
             expiredAt = tokenGenerator.getRefreshExpiredTime().toLocalDateTime()
         )
         refreshTokenRepository.save(refreshTokenEntity)
+
+        return TokenResDto(accessToken, refreshToken, accessExpiredTime)
+    }
+
+    @Transactional(rollbackFor = [Exception::class])
+    fun reissueToken(refreshToken: String): TokenResDto {
+        val refreshTokenEntity = (refreshTokenRepository.findByIdOrNull(refreshToken)
+            ?: throw InvalidTokenException())
+
+        if (LocalDateTime.now().isAfter(refreshTokenEntity.expiredAt))
+            throw ExpiredTokenException()
+
+        val accessToken = tokenGenerator.generateAccessToken(refreshTokenEntity.userId.toString())
+        val refreshToken = tokenGenerator.generateRefreshToken(refreshTokenEntity.userId.toString())
+        val accessExpiredTime = tokenGenerator.getAccessExpiredTime()
+
+        val newRefreshTokenEntity = RefreshToken(
+            userId = refreshTokenEntity.userId,
+            userToken = refreshToken,
+            expiredAt = tokenGenerator.getRefreshExpiredTime().toLocalDateTime()
+        )
+        refreshTokenRepository.save(newRefreshTokenEntity)
 
         return TokenResDto(accessToken, refreshToken, accessExpiredTime)
     }
